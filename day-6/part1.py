2ba7322be8ffacff696c15d4830f8eb5c30076e5
chiptopher <chrisboyerdev@gmail.com>
2022-12-06 09:22:14 -0500

    Day 6

---
+++ day-6/part1.py
@@ -0,0 +1,7 @@
+SEQUENCE_SIZE = 4
+
+with open('day-6/input.txt', 'r') as source:
+    data = source.read().splitlines()[0]
+    chunks = [data[i:i + SEQUENCE_SIZE] for i in range(len(data))]
+    print(next(index for (index, chunk) in enumerate(chunks) if len(set(chunk)) == SEQUENCE_SIZE) + SEQUENCE_SIZE)
+
